491152f00aacbb9b745937d4a3f6c8611af769df
KSuchita <kondetisuchita@gmail.com>
2025-12-05 20:11:00 +0530

    FIX: Removed conflicting Flask server run command for Streamlit deployment.

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def predict():
         return jsonify({'error': f"Prediction error: {e}"})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=5000)

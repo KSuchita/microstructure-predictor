90acd8777ea1c87718db4fd1cb85170829e532ff
KSuchita <kondetisuchita@gmail.com>
2025-12-06 08:38:37 +0530

    Update app.py, add requirements.txt and fix Railway deployment

--- app.py
+++ app.py
@@ -131,3 +131,7 @@ def predict():
 
     except Exception as e:
         return jsonify({'error': f"Prediction error: {e}"})
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=8080)
